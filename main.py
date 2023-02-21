54d3830d9ae5c7753dbe4e170c40b5ecce136a32
KshitijDabral <43210887+KshitijDabral@users.noreply.github.com>
2023-02-21 23:06:25 +0530

    Update main.py
    comments added

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import hashlib
 def hashgenerator(data):
     result = hashlib.sha256(data.encode())
     return result.hexdigest()
-
+#defines blockchain structure
 class Block:
     def __init__(self,data,hash,prev_hash):
         self.data=data
         self.hash=hash
         self.prev_hash=prev_hash
-
+#creates genesis block and adds block
 class Blockchain:
     def __init__(self):
         hashlast=hashgenerator('squirrel')
